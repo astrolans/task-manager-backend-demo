a680d4a42f5f7c1ca43a45bcdabc4e4b9ea7fcd8
Thomas Liu <s192287@student.hb.se>
2021-08-22 15:49:39 +0200

    Added baseentity.

---
+++ Lab2.Entities/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+    }
+}
---
+++ Lab2.Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Lab2.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Entities
+{
+    public class User : BaseEntity
+    {
+
+    }
+}
