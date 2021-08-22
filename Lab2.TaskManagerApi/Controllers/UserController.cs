bd77d6d3e5dbda3144318e5cd0d3471585216714
Thomas Liu <s192287@student.hb.se>
2021-08-22 21:02:10 +0200

    added usercontroller.

---
+++ Lab2.TaskManagerApi/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Lab2.Entities;
+using Lab2.TaskManagerApi.Servies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.TaskManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public UserController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPut]
+        public IActionResult Add([FromBody] User user)
+        {
+            return Ok();
+        }
+    }
+}
