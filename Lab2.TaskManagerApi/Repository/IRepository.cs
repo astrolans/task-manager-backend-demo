ba09dd1ff29ecfc44c66e3a13ad908bb7bd9cb60
Thomas Liu <s192287@student.hb.se>
2021-08-22 19:46:28 +0200

    added controller, repo and irepo

---
+++ Lab2.TaskManagerApi/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.TaskManagerApi.Repository
+{
+    interface IRepository
+    {
+    }
+}
---
+++ Lab2.TaskManagerApi/Repository/Repository.cs
@@ -0,0 +1,23 @@
+﻿using Lab2.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.TaskManagerApi.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly TaskManagerContext context;
+
+        public Repository(TaskManagerContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Lab2.Entities.Task>> GetAllTasks
+        {
+
+        }
+    }
+}
