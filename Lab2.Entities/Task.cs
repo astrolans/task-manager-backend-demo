c41ddecc7f38decb0057fd1a32387cb2a61c3f8c
Thomas Liu <s192287@student.hb.se>
2021-08-22 15:53:26 +0200

    added task model

---
+++ Lab2.Entities/Task.cs
@@ -0,0 +1,21 @@
+﻿using Lab2.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Entities
+{
+    public class Task : BaseEntity
+    {
+
+        public DateTime BeginDateTime { get; set; }
+
+        public DateTime DeadlineDateTime { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Requirements { get; set; } = string.Empty;
+    }
+}
