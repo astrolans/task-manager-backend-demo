6272f624c6d6f65b1b231ca74d0933084c9e1a8c
Thomas Liu <s192287@student.hb.se>
2021-08-22 17:39:08 +0200

    Added dbcontext to service and nugets.

--- Lab2.TaskManagerApi/Startup.cs
+++ Lab2.TaskManagerApi/Startup.cs
@@ -1,7 +1,9 @@
+using Lab2.DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace Lab2.TaskManagerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskManagerContext>(options => options
+                .UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
