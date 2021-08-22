9dd4b867375718fe50fbf307cfd574777755c54b
Thomas Liu <s192287@student.hb.se>
2021-08-22 16:13:09 +0200

    added dbcontext

---
+++ Lab2.DataAccessLayer/TaskManagerContext.cs
@@ -0,0 +1,20 @@
+﻿using Lab2.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.DataAccessLayer
+{
+    public class TaskManagerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Entities.Task> Tasks { get; set; }
+
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options)
+            : base(options) { }
+    }
+}
