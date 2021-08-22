9775bfab6ef99fc30c22dd5fc11857a28c604c27
Thomas Liu <s192287@student.hb.se>
2021-08-22 16:15:58 +0200

    added dbcontext factory and appsettings.

---
+++ Lab2.DataAccessLayer/TaskManagerContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.DataAccessLayer
+{
+    public class TaskManagerContextFactory : IDesignTimeDbContextFactory<TaskManagerContext>
+    {
+        public TaskManagerContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TaskManagerContext>();
+            optionsBuilder
+                 // Uncomment the following line if you want to print generated
+                 // SQL statements on the console.
+                 .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            return new TaskManagerContext(optionsBuilder.Options);
+        }
+    }
+}
