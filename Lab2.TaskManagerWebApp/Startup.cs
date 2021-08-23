2e1700dba1bb371432018c9769026067914f01d2
Thomas Liu <s192287@student.hb.se>
2021-08-23 15:12:43 +0200

    modified connectionString and added services.

--- Lab2.TaskManagerWebApp/Startup.cs
+++ Lab2.TaskManagerWebApp/Startup.cs
@@ -1,6 +1,8 @@
+using Lab2.DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,12 +25,23 @@ namespace Lab2.TaskManagerWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskManagerContext>(options => options
+                .UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddCors(options => options
+                .AddDefaultPolicy(builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()));
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
